101d9752cca8b4552f6e326125b24a6161a60e71
Mathla Alwahaibi <mathlaalwahaibi95@gmail.com>
2025-08-06 15:29:35 +0400

    Add Booking class for hotel management system

---
+++ Models/Booking.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelRoomManagementProject_LayeredArchitecture.Models
+{
+    public class Booking
+    {
+        [Key]
+        public  int BookingId { get; set; } // Unique identifier for the booking
+        [ForeignKey("Room")]
+        
+        public int RoomId { get; set; } // Foreign key to the Room table
+
+        [ForeignKey("Guest")]
+        public int GuestId { get; set; } // Foreign key to the Guest table
+
+        [Required]
+        [DataType(DataType.Date)]
+        public DateTime CheckInDate { get; set; } // Date of check-in
+
+        [Required]
+        [DataType(DataType.Date)]
+        
+        public DateTime CheckOutDate { get; set; } // Date of check-out
+
+        [NotMapped] // This property is not mapped to the database
+        // Represents the duration of the stay in days
+        public int StayDurationDays => (CheckOutDate - CheckInDate).Days;
+
+
+        public double TotalAmount { get; set; } // Total amount for the booking
+    }
+}
