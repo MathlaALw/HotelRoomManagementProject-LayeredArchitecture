2bf3609e5ef53aa3886ec862e54fb4fb17334acb
Mathla Alwahaibi <mathlaalwahaibi95@gmail.com>
2025-08-06 14:37:05 +0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿namespace HotelRoomManagementProject_LayeredArchitecture
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine("\nHotel Room Management System");
+                Console.WriteLine("1. Add New Room");
+                Console.WriteLine("2. View All Rooms");
+                Console.WriteLine("3. Reserve a Room");
+                Console.WriteLine("4. View All Reservations");
+                Console.WriteLine("5. Search Reservation by Guest Name");
+                Console.WriteLine("6. Find Highest Paying Guest");
+                Console.WriteLine("7. Cancel Reservation by Room Number");
+                Console.WriteLine("0. Exit");
+                Console.Write("Choose an option: ");
+                string choice = Console.ReadLine();
+
+                switch (choice)
+                {
+                    case "1": break;
+                    case "2": break;
+                    case "3": break;
+                    case "4": break;
+                    case "5": break;
+                    case "6": break;
+                    case "7": break;
+                    case "0":
+                       
+                        return; // Exit the system
+                    default: Console.WriteLine("Invalid choice. Try again."); break;
+                }
+            }
+
+
+
+        }
+    
+    
+    }
+}
