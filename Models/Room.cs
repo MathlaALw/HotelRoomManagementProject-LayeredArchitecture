07ba0a341f7560b5d28525320aeadecbdd494fb0
Mathla Alwahaibi <mathlaalwahaibi95@gmail.com>
2025-08-06 14:50:17 +0400

    Add Room class to manage hotel room details

---
+++ Models/Room.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelRoomManagementProject_LayeredArchitecture.Models
+{
+    public class Room
+    {
+        [Key]
+        public int RoomId { get; set; } // Unique identifier for the room
+        [Required]
+        public int RoomNumber { get; set; }
+        [Required]
+        [EnumDataType(typeof(roomType))]
+        public string RoomType { get; set; } // Single, Double, Suite
+        public decimal dailyRate { get; set; }
+        public bool IsAvailable { get; set; } = true; // Default to available
+       
+    }
+}
