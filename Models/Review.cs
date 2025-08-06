2c9a24660e30c7f133a1bab4cb3443c25dc520ba
Mathla Alwahaibi <mathlaalwahaibi95@gmail.com>
2025-08-06 15:29:48 +0400

    Add Review class to manage guest room feedback

---
+++ Models/Review.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelRoomManagementProject_LayeredArchitecture.Models
+{
+    public class Review
+    {
+
+        public int ReviewId { get; set; }
+        public int GuestId { get; set; }
+        public int RoomId { get; set; }
+        public string Comments { get; set; }
+        public int Rating { get; set; } // Assuming rating is an integer (e.g., 1 to 5)
+        public DateTime ReviewDate { get; set; }
+        // Navigation properties
+        public virtual Guest Guest { get; set; }
+        public virtual Room Room { get; set; }
+    }
+}
