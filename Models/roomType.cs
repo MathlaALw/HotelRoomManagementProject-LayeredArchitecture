0db1422945a971d36d207ae13b9f93fe37612f00
Mathla Alwahaibi <mathlaalwahaibi95@gmail.com>
2025-08-06 14:50:29 +0400

    Add roomType enum for hotel room classifications

---
+++ Models/roomType.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelRoomManagementProject_LayeredArchitecture.Models
+{
+    // This enum represents the different types of rooms available in the hotel.
+    public enum roomType
+    {
+
+        Single,
+        Double, 
+        Suite
+    }
+}
