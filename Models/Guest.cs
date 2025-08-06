81ac70b981bc545d7ec654b3c95596a8ed0ae568
Mathla Alwahaibi <mathlaalwahaibi95@gmail.com>
2025-08-06 14:58:05 +0400

    Add Guest class with validation for properties

---
+++ Models/Guest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelRoomManagementProject_LayeredArchitecture.Models
+{
+    public class Guest
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [RegularExpression(@"^[a-zA-Z\s]+$", ErrorMessage = "Name can only contain letters and spaces.")]
+        public string Name { get; set; }
+        [Required]
+        [RegularExpression(@"^[0-9]\d{7}$", ErrorMessage = "Contact number must be 8 digits long and contain only numbers.")]
+        public string ContactNumber { get; set; }
+
+
+    }
+}
