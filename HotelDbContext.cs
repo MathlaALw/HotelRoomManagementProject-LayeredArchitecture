679dbb7f99d294ad811923ec0faefe45aec265b1
Mathla Alwahaibi <mathlaalwahaibi95@gmail.com>
2025-08-11 14:30:04 +0400

    Add HotelDbContext for database management
    Introduces the HotelDbContext class inheriting from DbContext to configure the database context for the application. Sets up a connection to a SQL Server database named BankingDB and defines DbSet properties for guests, rooms, bookings, and reviews. Includes necessary using directives for required namespaces.

---
+++ HotelDbContext.cs
@@ -0,0 +1,27 @@
+﻿using HotelRoomManagementProject_LayeredArchitecture.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace HotelRoomManagementProject_LayeredArchitecture
+{
+    class HotelDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=(local);Initial Catalog=BankingDB;Integrated Security=True;TrustServerCertificate=True");
+        }
+
+        public DbSet<Guest> guests { get; set; }
+        public DbSet<Room>  rooms { get; set; }
+
+        public DbSet<Booking> bookings { get; set; }
+
+        public DbSet<Review> reviews { get; set; }
+
+    }
+}
